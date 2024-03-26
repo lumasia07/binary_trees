ae4234383ce3fa29c95365314175a5a9ea2f3a8a
lumasia07 <lumasiastancelaus@gmail.com>
2024-03-26 01:50:36 -0700

    fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -25,3 +25,22 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (h_right + 1);
 	}
 }
+
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to root node
+ * Return: 0 if NULL
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	size_t h_left, h_right;
+
+	if (tree == NULL)
+		return (0);
+
+	h_left = binary_tree_height(tree->left);
+	h_right = binary_tree_height(tree->right);
+
+	return (h_left - h_right);
+}
